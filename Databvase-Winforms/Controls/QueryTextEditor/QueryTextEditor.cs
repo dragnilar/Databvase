95a26a002f5aa7cbf7807a46d6021a67f217bea1
Great Dead Cthulhu <dragnilar@gmail.com>
2018-04-17 02:31:34 -0400

    Added sql syntax highlighting service

--- Databvase-Winforms/Controls/QueryTextEditor/QueryTextEditor.cs
+++ Databvase-Winforms/Controls/QueryTextEditor/QueryTextEditor.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using Databvase_Winforms.Messages;
+using Databvase_Winforms.Services;
 using DevExpress.Mvvm;
 using DevExpress.XtraEditors;
 using DevExpress.XtraRichEdit;
 using DevExpress.XtraRichEdit.API.Native;
+using DevExpress.XtraRichEdit.Commands;
+using DevExpress.XtraRichEdit.Menu;
+using DevExpress.CodeParser;
+using DevExpress.XtraRichEdit.Services;
 
 namespace Databvase_Winforms.Controls.QueryTextEditor
 {
@@ -13,21 +18,52 @@ namespace Databvase_Winforms.Controls.QueryTextEditor
         public QueryTextEditor()
         {
             InitializeQueryTextEditor();
-            RegisterMessages();
+
         }
 
         private void InitializeQueryTextEditor()
         {
-            SetBackgroundColor();
             ShowLineNumbers();
-            Enter += OnEnter; //TODO -This feels kind of hack-ish, but its the closest we have to "Shown"
+            RegisterMessages();
+            RegisterServices();
+            HookUpEvents();
         }
 
+        private void RegisterMessages()
+        {
+            Messenger.Default.Register<SettingsUpdatedMessage>(this, SettingsUpdatedMessage.SettingsUpdatedSender, ApplySettingsUpdate);
+        }
+
+        private void RegisterServices()
+        {
+            ReplaceService<ISyntaxHighlightService>(new SQLSyntaxHighlightingService(Document));
+        }
+
+        private void HookUpEvents()
+        {
+            Enter += OnEnter; //TODO -This feels kind of hack-ish, but its the closest we have to "Shown"
+            PopupMenuShowing += QueryTextEditor_PopupMenuShowing;
+        }
+
+
+
+        #region events
         private void OnEnter(object sender, EventArgs eventArgs)
         {
             if (WasShown) return;
             SetBackgroundColor();
             WasShown = true;
+        }
+
+        private void QueryTextEditor_PopupMenuShowing(object sender, PopupMenuShowingEventArgs e)
+        {
+            if (e.MenuType == RichEditMenuType.Text)
+            {
+                e.Menu.RemoveMenuItem(RichEditCommandId.ShowNumberingListForm);
+                e.Menu.RemoveMenuItem(RichEditCommandId.CreateBookmark);
+                e.Menu.RemoveMenuItem(RichEditCommandId.CreateHyperlink);
+                e.Menu.RemoveMenuItem(RichEditCommandId.NewCommentContentMenu);
+            }
         }
 
         public void ShowLineNumbers()
@@ -49,12 +85,7 @@ namespace Databvase_Winforms.Controls.QueryTextEditor
             Document.CharacterStyles["Line Number"].ForeColor = App.Config.TextEditorLineNumberColor;
         }
 
-        private void RegisterMessages()
-        {
-            Messenger.Default.Register<SettingsUpdatedMessage>(this, SettingsUpdatedMessage.SettingsUpdatedSender, ApplysettingsUpdate);
-        }
-
-        private void ApplysettingsUpdate(SettingsUpdatedMessage message)
+        private void ApplySettingsUpdate(SettingsUpdatedMessage message)
         {
             if (message.Type == SettingsUpdatedMessage.SettingsUpdateType.TextEditorBackground)
             {
@@ -62,5 +93,7 @@ namespace Databvase_Winforms.Controls.QueryTextEditor
                 SetLineColor();
             }
         }
+
+        #endregion
     }
 }
---
+++ Databvase-Winforms/Services/SQLSyntaxHighlightingService.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using DevExpress.XtraRichEdit.API.Native;
+using DevExpress.XtraRichEdit.Services;
+
+namespace Databvase_Winforms.Services
+{
+    /// <summary>
+    /// Source: https://www.devexpress.com/Support/Center/Example/Details/E4139/how-to-implement-t-sql-language-syntax-highlighting-by-creating-syntax-highlight-tokens
+    /// </summary>
+    internal class SQLSyntaxHighlightingService : ISyntaxHighlightService
+    {
+        #region #parsetokens
+
+        private readonly Document document;
+
+        private readonly SyntaxHighlightProperties defaultSettings =
+            new SyntaxHighlightProperties {ForeColor = Color.Black};
+
+        private readonly SyntaxHighlightProperties keywordSettings =
+            new SyntaxHighlightProperties {ForeColor = Color.Blue};
+
+        private readonly SyntaxHighlightProperties stringSettings =
+            new SyntaxHighlightProperties {ForeColor = Color.Green};
+
+        private readonly string[] keywords =
+        {
+            "INSERT", "SELECT", "CREATE", "TABLE", "USE", "IDENTITY", "ON", "OFF", "NOT", "NULL", "WITH", "SET",
+            "FROM", "WHERE", "JOIN", "DBCC"
+        };
+
+        public SQLSyntaxHighlightingService(Document document)
+        {
+            this.document = document;
+        }
+
+        private List<SyntaxHighlightToken> ParseTokens()
+        {
+            var tokens = new List<SyntaxHighlightToken>();
+            DocumentRange[] ranges = null;
+            // search for quotation marks
+            ranges = document.FindAll("'", SearchOptions.None);
+            for (var i = 0; i < ranges.Length / 2; i++)
+                tokens.Add(new SyntaxHighlightToken(ranges[i * 2].Start.ToInt(),
+                    ranges[i * 2 + 1].Start.ToInt() - ranges[i * 2].Start.ToInt() + 1, stringSettings));
+            // search for keywords
+            for (var i = 0; i < keywords.Length; i++)
+            {
+                ranges = document.FindAll(keywords[i], SearchOptions.None | SearchOptions.WholeWord);
+
+                for (var j = 0; j < ranges.Length; j++)
+                    if (!IsRangeInTokens(ranges[j], tokens))
+                        tokens.Add(new SyntaxHighlightToken(ranges[j].Start.ToInt(), ranges[j].Length,
+                            keywordSettings));
+            }
+
+            // order tokens by their start position
+            tokens.Sort(new SyntaxHighlightTokenComparer());
+            // fill in gaps in document coverage
+            AddPlainTextTokens(tokens);
+            return tokens;
+        }
+
+        private void AddPlainTextTokens(List<SyntaxHighlightToken> tokens)
+        {
+            var count = tokens.Count;
+            if (count == 0)
+            {
+                tokens.Add(new SyntaxHighlightToken(0, document.Range.End.ToInt(), defaultSettings));
+                return;
+            }
+
+            tokens.Insert(0, new SyntaxHighlightToken(0, tokens[0].Start, defaultSettings));
+            for (var i = 1; i < count; i++)
+                tokens.Insert(i * 2, new SyntaxHighlightToken(tokens[i * 2 - 1].End,
+                    tokens[i * 2].Start - tokens[i * 2 - 1].End, defaultSettings));
+            tokens.Add(new SyntaxHighlightToken(tokens[count * 2 - 1].End,
+                document.Range.End.ToInt() - tokens[count * 2 - 1].End, defaultSettings));
+        }
+
+        private bool IsRangeInTokens(DocumentRange range, List<SyntaxHighlightToken> tokens)
+        {
+            return tokens.Any(t => IsIntersect(range, t));
+        }
+
+        private bool IsIntersect(DocumentRange range, SyntaxHighlightToken token)
+        {
+            var start = range.Start.ToInt();
+            if (start >= token.Start && start < token.End)
+                return true;
+            var end = range.End.ToInt() - 1;
+            if (end >= token.Start && end < token.End)
+                return true;
+            return false;
+        }
+
+        #endregion #parsetokens
+
+        #region #ISyntaxHighlightServiceMembers
+
+        public void ForceExecute()
+        {
+            Execute();
+        }
+
+        public void Execute()
+        {
+            document.ApplySyntaxHighlight(ParseTokens());
+        }
+
+        #endregion #ISyntaxHighlightServiceMembers
+    }
+
+    #region #SyntaxHighlightTokenComparer
+
+    public class SyntaxHighlightTokenComparer : IComparer<SyntaxHighlightToken>
+    {
+        public int Compare(SyntaxHighlightToken x, SyntaxHighlightToken y)
+        {
+            return x.Start - y.Start;
+        }
+    }
+
+    #endregion #SyntaxHighlightTokenComparer
+}
