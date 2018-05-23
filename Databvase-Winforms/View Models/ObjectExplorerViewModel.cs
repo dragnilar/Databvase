5f0298771e201cc3a665c11ed9a7e356441eb499
Great Dead Cthulhu <dragnilar@gmail.com>
2018-05-22 23:00:46 -0400

    Ran R# code cleanup on ObjectExplorerViewModel

--- Databvase-Winforms/View Models/ObjectExplorerViewModel.cs
+++ Databvase-Winforms/View Models/ObjectExplorerViewModel.cs
@@ -1,37 +1,32 @@
-﻿ using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
- using System.Diagnostics;
- using System.Linq;
-using System.Threading.Tasks;
- using System.Windows;
- using Databvase_Winforms.Globals;
+using System.Linq;
+using System.Windows;
 using Databvase_Winforms.Messages;
 using Databvase_Winforms.Models;
 using Databvase_Winforms.Models.Data_Providers;
-using Databvase_Winforms.Services;
- using Databvase_Winforms.Services.Window_Dialog_Services;
- using DevExpress.Mvvm;
+using Databvase_Winforms.Services.Window_Dialog_Services;
+using DevExpress.Mvvm;
 using DevExpress.Mvvm.DataAnnotations;
 using DevExpress.Mvvm.POCO;
 using DevExpress.XtraTreeList;
 using DevExpress.XtraTreeList.Nodes;
-using Microsoft.SqlServer.Management.Smo;
 
 namespace Databvase_Winforms.View_Models
 {
     [POCOViewModel]
     public class ObjectExplorerViewModel
     {
+        public enum UnboundLoadModes
+        {
+            NotLoading,
+            BeginUnboundLoad,
+            FinishUnboundLoad,
+            FinishUnboundLoadRefresh
+        }
 
-        private ObjectExplorerDataSource _dataSourceModel;
-        public virtual UnboundLoadModes LoadingMode { get; set; }
-        public virtual string SelectTopContextMenuItemDescription { get; set; }
-        public virtual BindingList<ObjectExplorerNode> ObjectExplorerSource { get; set; }
-        protected IMessageBoxService MessageBoxService => this.GetService<IMessageBoxService>();
-        protected IBackupViewService BackupViewService => this.GetService<IBackupViewService>();
-        public virtual ObjectExplorerNode FocusedNode { get; set; }
-        private bool _refreshing = false;
+        private readonly ObjectExplorerDataSource _dataSourceModel;
+        private bool _refreshing;
 
         public ObjectExplorerViewModel()
         {
@@ -42,6 +37,12 @@ namespace Databvase_Winforms.View_Models
             LoadingMode = UnboundLoadModes.NotLoading;
         }
 
+        public virtual UnboundLoadModes LoadingMode { get; set; }
+        public virtual string SelectTopContextMenuItemDescription { get; set; }
+        public virtual BindingList<ObjectExplorerNode> ObjectExplorerSource { get; set; }
+        protected IMessageBoxService MessageBoxService => this.GetService<IMessageBoxService>();
+        protected IBackupViewService BackupViewService => this.GetService<IBackupViewService>();
+        public virtual ObjectExplorerNode FocusedNode { get; set; }
 
 
         private void GetSelectTopDescriptionForPopupMenu()
@@ -71,8 +72,8 @@ namespace Databvase_Winforms.View_Models
             if (message == null) return;
             LoadingMode = UnboundLoadModes.BeginUnboundLoad;
             App.Connection.DisconnectCurrentInstance(); //TODO - See if there's a better way to do this...
-            var instanceTree =  _dataSourceModel.DataSource.Where(r => r.InstanceName == message.InstanceName).ToList();
-            foreach (var item in instanceTree)  _dataSourceModel.DataSource.Remove(item);
+            var instanceTree = _dataSourceModel.DataSource.Where(r => r.InstanceName == message.InstanceName).ToList();
+            foreach (var item in instanceTree) _dataSourceModel.DataSource.Remove(item);
             FinishUnboundLoad();
         }
 
@@ -85,120 +86,6 @@ namespace Databvase_Winforms.View_Models
         {
             BackupViewService.Show();
         }
-
-        #region Object Explorer On Demand Data Methods
-
-        public void RefreshNode()
-        {
-            var node = FocusedNode;
-            try
-            {
-                _refreshing = true;
-                LoadingMode = UnboundLoadModes.BeginUnboundLoad;
-                _dataSourceModel.RefreshNode(node);
-                FinishUnboundLoad();
-            }
-            catch (Exception e)
-            {
-                _dataSourceModel.CreateEmptyNode(node);
-                DisplayErrorMessage(e.Message, "Error Refreshing Objects");
-            }
-
-        }
-
-        private void GetInstances(InstanceConnectedMessage message)
-        {
-            if (message == null) return;
-            try
-            {
-                _dataSourceModel.GenerateInstances();
-            }
-            catch (Exception e)
-            {
-                DisplayErrorMessage(e.Message, "Error Getting Objects");
-            }
-        }
-
-        public void ObjectExplorer_OnBeforeExpand(BeforeExpandEventArgs e)
-        {
-            if (_refreshing)
-            {
-                return;
-            }
-
-            if (e.Node.Nodes.Count <= 0)
-            {
-                var model = GetModelForNode(e.Node);
-                LoadDataForObjectExplorerDynamically(model);
-            }
-        }
-
-        private void LoadDataForObjectExplorerDynamically(ObjectExplorerNode selectedNode)
-        {
-            LoadingMode = UnboundLoadModes.BeginUnboundLoad;
-            try
-            {
-                _dataSourceModel.GetNodes(selectedNode);
-                FinishUnboundLoad();
-            }
-            catch (Exception e)
-            {
-                _dataSourceModel.CreateEmptyNode(selectedNode);
-                DisplayErrorMessage(e.Message, "Error Getting Objects");
-            }
-
-        }
-
-        private ObjectExplorerNode GetModelForNode(TreeListNode node)
-        {
-            var model = node.TreeList.GetRow(node.Id) as ObjectExplorerNode;
-            return model;
-        }
-
-
-
-
-
-        #endregion
-
-        #region Scripting
-
-        public void NewQueryScript()
-        {
-            var selectedDatabase = FocusedNode.GetDatabaseFromNode();
-            var selectedDatabaseName = selectedDatabase == null ? string.Empty : selectedDatabase.Name;
-            SendBlankScript(selectedDatabaseName);
-        }
-
-        public void ScriptSelectTopForObjectExplorerData()
-        {
-            new ScriptGenerator().GenerateSelectTopStatement(FocusedNode);
-        }
-
-        public void ScriptSelectAllForObjectExplorerData()
-        {
-            new ScriptGenerator().GenerateSelectAllStatement(FocusedNode);
-
-        }
-
-        public void ScriptModifyForObjectExplorerData()
-        {
-            new ScriptGenerator().GenerateModifyScript(FocusedNode);
-
-        }
-
-        public void ScriptAlterForObjectExplorerData()
-        {
-            new ScriptGenerator().GenerateAlterScript(FocusedNode);
-
-        }
-
-        private void SendBlankScript(string selectedDatabaseName)
-        {
-            new ScriptGenerator().GenerateBlankScript(selectedDatabaseName);
-        }
-
-        #endregion
 
         #region Focused Node Changed
 
@@ -232,12 +119,106 @@ namespace Databvase_Winforms.View_Models
             _refreshing = false;
         }
 
-        public enum UnboundLoadModes
+        #region Object Explorer On Demand Data Methods
+
+        public void RefreshNode()
         {
-            NotLoading,
-            BeginUnboundLoad,
-            FinishUnboundLoad,
-            FinishUnboundLoadRefresh
+            var node = FocusedNode;
+            try
+            {
+                _refreshing = true;
+                LoadingMode = UnboundLoadModes.BeginUnboundLoad;
+                _dataSourceModel.RefreshNode(node);
+                FinishUnboundLoad();
+            }
+            catch (Exception e)
+            {
+                _dataSourceModel.CreateEmptyNode(node);
+                DisplayErrorMessage(e.Message, "Error Refreshing Objects");
+            }
         }
+
+        private void GetInstances(InstanceConnectedMessage message)
+        {
+            if (message == null) return;
+            try
+            {
+                _dataSourceModel.GenerateInstances();
+            }
+            catch (Exception e)
+            {
+                DisplayErrorMessage(e.Message, "Error Getting Objects");
+            }
+        }
+
+        public void ObjectExplorer_OnBeforeExpand(BeforeExpandEventArgs e)
+        {
+            if (_refreshing) return;
+
+            if (e.Node.Nodes.Count <= 0)
+            {
+                var model = GetModelForNode(e.Node);
+                LoadDataForObjectExplorerDynamically(model);
+            }
+        }
+
+        private void LoadDataForObjectExplorerDynamically(ObjectExplorerNode selectedNode)
+        {
+            LoadingMode = UnboundLoadModes.BeginUnboundLoad;
+            try
+            {
+                _dataSourceModel.GetNodes(selectedNode);
+                FinishUnboundLoad();
+            }
+            catch (Exception e)
+            {
+                _dataSourceModel.CreateEmptyNode(selectedNode);
+                DisplayErrorMessage(e.Message, "Error Getting Objects");
+            }
+        }
+
+        private ObjectExplorerNode GetModelForNode(TreeListNode node)
+        {
+            var model = node.TreeList.GetRow(node.Id) as ObjectExplorerNode;
+            return model;
+        }
+
+        #endregion
+
+        #region Scripting
+
+        public void NewQueryScript()
+        {
+            var selectedDatabase = FocusedNode.GetDatabaseFromNode();
+            var selectedDatabaseName = selectedDatabase == null ? string.Empty : selectedDatabase.Name;
+            SendBlankScript(selectedDatabaseName);
+        }
+
+        public void ScriptSelectTopForObjectExplorerData()
+        {
+            new ScriptGenerator().GenerateSelectTopStatement(FocusedNode);
+        }
+
+        public void ScriptSelectAllForObjectExplorerData()
+        {
+            new ScriptGenerator().GenerateSelectAllStatement(FocusedNode);
+        }
+
+        public void ScriptModifyForObjectExplorerData()
+        {
+            new ScriptGenerator().GenerateModifyScript(FocusedNode);
+        }
+
+        public void ScriptAlterForObjectExplorerData()
+        {
+            new ScriptGenerator().GenerateAlterScript(FocusedNode);
+        }
+
+        private void SendBlankScript(string selectedDatabaseName)
+        {
+            new ScriptGenerator().GenerateBlankScript(selectedDatabaseName);
+        }
+
+        #endregion
     }
 }
